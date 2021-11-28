f0a7b8d3eeed6737b1fab305fe2fdbf5cc3c6001
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-11-28 21:04:49 +0200

    DateOnlyTimeOnly.AspNet.Swashbuckle

---
+++ DateOnlyTimeOnly.AspNet.Swashbuckle/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class SwaggerGenOptionsExtensions
+{
+    /// <summary>
+    /// Maps DateOnly and TimeOnly to string.
+    /// </summary>
+    public static void UseDateOnlyTimeOnlyStringConverters(this SwaggerGenOptions options)
+    {
+        options.MapType<DateOnly>(() => new OpenApiSchema 
+        { 
+            Type = "string", 
+            Format = "date" 
+        });
+        options.MapType<TimeOnly>(() => new OpenApiSchema 
+        { 
+            Type = "string", 
+            Format = "time", 
+            Example = OpenApiAnyFactory.CreateFromJson("\"0:11:42\"") 
+        });
+    }
+}
