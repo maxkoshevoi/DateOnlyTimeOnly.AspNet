2515ab26d32c4b5215a7685c3aab78cd8d2ec5ad
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2022-10-14 01:14:43 +0300

    Net 7 migration path

--- DateOnlyTimeOnly.AspNet/Extensions/JsonOptionsExtensions.cs
+++ DateOnlyTimeOnly.AspNet/Extensions/JsonOptionsExtensions.cs
@@ -3,15 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
-public static class JsonOptionsExtensions
+public static partial class JsonOptionsExtensions
 {
+    public static partial JsonOptions UseDateOnlyTimeOnlyStringConverters(this JsonOptions options);
+
+#if NET6_0
     /// <summary>
     /// Adds <see cref="DateOnly"/> and <see cref="TimeOnly"/> serializers to System.Text.Json.
     /// </summary>
-    public static JsonOptions UseDateOnlyTimeOnlyStringConverters(this JsonOptions options)
+    public static partial JsonOptions UseDateOnlyTimeOnlyStringConverters(this JsonOptions options)
     {
         options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
         options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverter());
         return options;
     }
+#endif
+
+#if NET7_0_OR_GREATER
+    /// <summary>
+    /// Adds <see cref="DateOnly"/> and <see cref="TimeOnly"/> serializers to System.Text.Json.
+    /// </summary>
+    [Obsolete("DateOnly/TimeOnly work out of the box in .NET 7+. You can remove call to this method.")]
+    public static partial JsonOptions UseDateOnlyTimeOnlyStringConverters(this JsonOptions options) => options;
+#endif
 }
